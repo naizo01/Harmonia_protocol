3cf7164b52cfd62d1758c06356c987520d92ecdd
naizo10 <naizo018767@gmail.com>
2024-11-16 16:05:55 +0700

    Add DynamicFeeScript for hook contract deployment

---
+++ contracts/script/00_DynamicFee.s.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "./base/DeployBase.sol";
+
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+
+import {Constants} from "./base/Constants.sol";
+import {DynamicFee} from "../src/DynamicFee.sol";
+import {HookMiner} from "../test/utils/HookMiner.sol";
+
+/// @notice Mines the address and deploys the DynamicFee.sol Hook contract
+contract DynamicFeeScript is DeployBase, Constants {
+    function setUp() public {}
+
+    function run() public {
+        address sp = readDeployedAddress("SP");
+        address pyth = readDeployedAddress("PYTH");
+        bytes32 pyth_price_feed_id = bytes32(uint256(0x78d185a741d07edb3412b09008b7c5cfb9bbbd7d568bf00ba737b456ba171501));
+
+        // hook contracts must have specific flags encoded in the address
+        uint160 flags = uint160(
+            Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_INITIALIZE_FLAG
+        );
+
+        // Mine a salt that will produce a hook address with the correct flags
+        bytes memory constructorArgs = abi.encode(
+            POOLMANAGER,
+            sp,
+            pyth,
+            pyth_price_feed_id,
+            false
+        );
+        (address hookAddress, bytes32 salt) = HookMiner.find(
+            CREATE2_DEPLOYER,
+            flags,
+            type(DynamicFee).creationCode,
+            constructorArgs
+        );
+
+        // Deploy the hook using CREATE2
+        vm.broadcast();
+        DynamicFee dynamicFee = new DynamicFee{salt: salt}(
+            IPoolManager(POOLMANAGER),
+            sp,
+            pyth,
+            pyth_price_feed_id,
+            false
+        );
+
+        writeDeployedAddress(address(dynamicFee), "DYNAMICFEE");
+
+        require(
+            address(dynamicFee) == hookAddress,
+            "DynamicFeeScript: hook address mismatch"
+        );
+    }
+}
