205b8dc475c72a1fb9c250124ae4749005b3a805
naizo10 <naizo018767@gmail.com>
2024-11-16 20:13:24 +0700

    Add new forge scripts for contract operations

---
+++ contracts/script/sh/00a.sh
@@ -0,0 +1,18 @@
+# .envファイルを読み込む
+if [ -f .env ]; then
+  export $(cat .env | xargs)
+else
+  echo ".envファイルが見つかりません。"
+  exit 1
+fi
+
+# forge scriptコマンドを実行
+forge script script/00_WhitelistHook.s.sol:WhitelistHookScript \
+  --rpc-url $RPC_URL \
+  --chain-id $CHAIN_ID \
+  --private-key $PRIVATE_KEY \
+  --broadcast \
+  -vvvv
+  # --verify \
+  # --verifier-url $SCAN_URL \
+  # --etherscan-api-key $SCAN_API_KEY \
---
+++ contracts/script/sh/01.sh
@@ -0,0 +1,17 @@
+# .envファイルを読み込む
+if [ -f .env ]; then
+  export $(cat .env | xargs)
+else
+  echo ".envファイルが見つかりません。"
+  exit 1
+fi
+
+# forge scriptコマンドを実行
+forge script script/01_CreatePoolAndMintLiquidity.s.sol:CreatePoolAndAddLiquidityScript \
+  --rpc-url $RPC_URL \
+  --chain-id $CHAIN_ID \
+  --verifier-url $SCAN_URL \
+  --private-key $PRIVATE_KEY \
+  --etherscan-api-key $SCAN_API_KEY \
+  --broadcast \
+  --verify -vvvv
